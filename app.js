dad7ec1b2dc60c52e93c833958dbd68f2324c8c9
softwareluke <softwareluke@softwarukedeAir.lan>
2017-12-09 16:59:20 +0800

    s

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'app')));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
